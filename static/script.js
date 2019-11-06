df15c54633ef1e531e18da6de7087310cd46d16f
Ben Junya <benjunya@Bens-MacBook-Pro.local>
2019-11-05 16:08:26 -0800

    Not very fun or interesting artwork right now

--- static/script.js
+++ static/script.js
@@ -1,9 +1,40 @@
-const boxes = Array.from(document.querySelectorAll('.box'));
+const TOTAL_ROWS = 40;
+const TOTAL_BOXES = 40;
 
-const onBoxClick = ({ currentTarget }) => {
-  currentTarget.classList.toggle('expand');
+const body = document.querySelector('.body');
 
+for (let r = 0; r < TOTAL_ROWS; r++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  
+  for (let i = 0; i < TOTAL_BOXES; i++) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('box-wrapper');
+    const box = document.createElement('div');
+    box.classList.add('box');
+  
+    wrapper.appendChild(box);
+    row.appendChild(wrapper);
+  }
+
+  body.appendChild(row);
 }
-boxes.forEach(box => {
-  box.addEventListener('click', onBoxClick);
-})
+
+
+const boxes = Array.from(document.querySelectorAll('.box'));
+const DURATION = 10000;
+const VARIANCE = 4;
+let left = 0;
+
+function loop() {
+  window.requestAnimationFrame(loop);
+  
+  const p = (Date.now() % DURATION) / 100;
+  boxes.forEach(box => {
+    let v = Math.floor(Math.random() * 100) % VARIANCE;
+    box.style = `left: ${(p + v) * 25}%;`;
+
+    box.parentElement.style = `margin-top: ${p % 100 / 10}%`;
+  });
+}
+// loop(); 
